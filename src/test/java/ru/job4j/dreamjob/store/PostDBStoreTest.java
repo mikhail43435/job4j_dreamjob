2b0a1fbea05653d7c169fb82f35257316e474d92
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2022-07-01 13:12:28 +0300

    bot check pass attempt 5

--- src/test/java/ru/job4j/dreamjob/store/PostDBStoreTest.java
+++ src/test/java/ru/job4j/dreamjob/store/PostDBStoreTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.dreamjob.store;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.job4j.dreamjob.Main;
 import ru.job4j.dreamjob.model.City;
