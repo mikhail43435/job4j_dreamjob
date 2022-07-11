077a024413de77bc6ef327f802e074827972ff46
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2022-07-11 19:28:07 +0300

    tiny update

--- src/main/java/ru/job4j/dreamjob/model/City.java
+++ src/main/java/ru/job4j/dreamjob/model/City.java
@@ -6,9 +6,6 @@ public class City {
     private int id;
     private String name;
 
-    public City() {
-    }
-
     public City(int id, String name) {
         this.id = id;
         this.name = name;
--- src/main/java/ru/job4j/dreamjob/util/UserHandler.java
+++ src/main/java/ru/job4j/dreamjob/util/UserHandler.java
@@ -18,3 +18,5 @@ public class UserHandler {
         return user;
     }
 }
+
+
